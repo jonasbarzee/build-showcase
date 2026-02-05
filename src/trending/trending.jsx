4be9104edb5bc678bfbc7133323fb4737dba58db
jonasbarzee <jonascbarzee@gmail.com>
2026-02-05 15:08:24 -0700

    add stubbed jsx files for components

---
+++ src/trending/trending.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function Trending() {
+  return (
+    <main className="container-fluid bg-secondary text-center">
+      <div>trending displayed here</div>
+    </main>
+  );
+}
