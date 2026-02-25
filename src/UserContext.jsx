4c1d1abfb489f8d9b7074fb09d70208c78eb83b9
jonasbarzee <jonascbarzee@gmail.com>
2026-02-24 21:32:11 -0700

    add routing to trending on login and root on logout

--- src/UserContext.jsx
+++ src/UserContext.jsx
@@ -1,5 +1,6 @@
 import React, { createContext, useContext } from "react";
 import { useLocalStorage } from "./hooks/useLocalStorage";
+import { useNavigate } from "react-router-dom";
 
 const UserContext = createContext();
 
@@ -8,15 +9,19 @@ export function UserProvider({ children }) {
 	const [password, setPassword] = useLocalStorage('password', '');
 	const isLoggedIn = !!username;
 
+	const navigate = useNavigate();
+
 	const login = (username, password) => {
 		setUsername(username);
 		setPassword(password);
+		navigate('/trending');
 
 	}
 
 	const logout = () => {
 		setUsername('');
 		setPassword('');
+		navigate('/');
 	}
 
 	return (
