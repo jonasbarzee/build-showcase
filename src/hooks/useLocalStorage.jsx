de55245ede86a76cabb20cdae5ec466a1a5cf376
Jonas Barzee <jonascbarzee@gmail.com>
2026-03-13 20:07:34 -0600

    fix voting bug in local storage

--- src/hooks/useLocalStorage.jsx
+++ src/hooks/useLocalStorage.jsx
@@ -14,6 +14,17 @@ export function useLocalStorage(key, initialValue) {
 
     useEffect(() => {
         try {
+            const item = window.localStorage.getItem(key);
+            setValue(item ? JSON.parse(item) : initialValue);
+        } catch (error) {
+            console.error("LocalStorage Read Error: ", error);
+            setValue(initialValue);
+        }
+
+    }, [key]);
+
+    useEffect(() => {
+        try {
             window.localStorage.setItem(key, JSON.stringify(value));
         } catch (error) {
             console.error("LocalStorage Write Error: ", error)
