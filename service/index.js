a44985598025fba122ca5cddd28ad956cdeab969
Jonas Barzee <jonascbarzee@gmail.com>
2026-03-13 18:55:04 -0600

    move mock posts to service layer, add upvote and downvote endpoint

--- service/index.js
+++ service/index.js
@@ -10,7 +10,63 @@ const authCookieName = 'token';
 
 // The scores and users are saved in memory and disappear whenever the service is restarted.
 let users = [];
-let posts = [];
+let posts = [{
+    id: 1,
+    upvotes: 0,
+    downvotes: 0,
+    category: 'gallery',
+    author: 'You',
+    imageUrl: 'https://media.istockphoto.com/id/1282514444/photo/cow-udder-large-and-full-and-with-horns-in-the-green-pasture-and-a-blue-sky.jpg?s=612x612&w=0&k=20&c=a2TuO1u4H4wKW7aSizBh7Df8CLA70MEPTcadLfc35bk=',
+    content: 'I finally completed my first project, a hotdog maker! I used a smart fridge and a GPU to make it work with custom RGB.'
+},
+{
+    id: 2,
+    upvotes: 0,
+    downvotes: 0,
+    category: 'gallery',
+    author: 'You',
+    imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR4t2ZwOPQaboirYn2FR6LU_CKJBBwblx1qsw&s',
+    content: "This is a car, I made a another one this time with big eyes and a silly, surprised expression on it's face. isn't it cute?"
+
+},
+{
+    id: 3,
+    upvotes: 0,
+    downvotes: 0,
+    category: 'trending',
+    author: 'Not You',
+    imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS0sOA9N5tXNCRWuSoihvRD4F4qPf6_30El1A&s',
+    content: 'I am so happy that my hydroponic bonzai tree wifi router project is finally working. I did it as a class project for the 15th grade.',
+},
+{
+    id: 4,
+    upvotes: 0,
+    downvotes: 0,
+    category: 'trending',
+    author: 'Also Not You',
+    imageUrl: 'https://wallpapers.com/images/hd/funny-horse-pictures-cv9t06tu59f6pjb1.jpg',
+    content: "I don't even like horses, but I he's cute so he's on here",
+},
+{
+    id: 5,
+    upvotes: 0,
+    downvotes: 0,
+    category: 'voting',
+    author: 'Not You',
+    imageUrl: 'https://www.royalplantscape.com/cdn/shop/collections/Buy_Plants_Online_Cheap_Prices.jpg?v=170368047',
+    content: 'After hours of work my rotating manticore 3d printer hot tub crossover is complete. Shoutout to mom and dad for telling me I was a failure.',
+},
+{
+    id: 6,
+    upvotes: 0,
+    downvotes: 0,
+    category: 'voting',
+    author: "Surprise, it's Not You",
+    imageUrl: 'https://cdn.pixabay.com/photo/2024/05/27/20/08/technical-8792191_640.jpg',
+    content: 'Here is a blueprint of a perfectly, over-engineeried screw or whatever this is. I hope you like it. I am running out of ideas.'
+
+}
+];
 
 // The service port. In production the front-end code is statically hosted by the service on the same port.
 const port = process.argv.length > 2 ? process.argv[2] : 4000;
@@ -79,19 +135,32 @@ const verifyAuth = async (req, res, next) => {
     }
 };
 
-// TODO Repurpose this function for getting the posts only if a user
-// is authenticated through their token
-// GetScores
+// GetPosts 
 apiRouter.get('/posts', verifyAuth, (_req, res) => {
     res.send(posts);
 });
 
-// TODO Repurpose this function for submitting a post only if a user
-// is authenticated through their token
-// SubmitScore
+// SubmitPost
 apiRouter.post('/posts', verifyAuth, (req, res) => {
     posts = updatePosts(req.body);
     res.send(posts);
+});
+
+apiRouter.put('/posts/:id/vote', verifyAuth, (req, res) => {
+    const postId = parseInt(req.params.id);
+    const { type, action } = req.body;
+
+    const postIndex = posts.findIndex(p => p.id === postId);
+    if (postIndex === -1) {
+        return res.status(404).send({ msg: 'Post not found' });
+    }
+    if (action === 'cast') {
+        posts[postIndex][type] += 1;
+    } else if (action === 'rescind') {
+        posts[postIndex][type] = Math.max(0, posts[postIndex][type] - 1);
+    }
+
+    res.send(posts[postIndex]);
 });
 
 // Default error handler
